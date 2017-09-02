c56d350d34bbaf70f8ec2cbc4baa3f7de12e153d
Ada Joule <ada.fulmina@gmail.com>
2017-09-02 20:18:50 +0700

    Remove wine alias

--- bash/config/.bashrc.d/wine.sh
+++ bash/config/.bashrc.d/wine.sh
@@ -1,7 +1,3 @@
 export WINEARCH=win32
 export WINEDEBUG=-all
 
-alias wine='env FREETYPE_PROPERTIES="truetype:interpreter-version=35" wine'
-alias winecfg='env FREETYPE_PROPERTIES="truetype:interpreter-version=35" winecfg'
-alias winetricks='env FREETYPE_PROPERTIES="truetype:interpreter-version=35" winetricks'
-
