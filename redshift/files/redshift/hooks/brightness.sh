fb5e4da1950a2a9073a359f71408ced0b261f2f2
Ada Joule <ada.fulmina@gmail.com>
2017-11-13 15:32:18 +0700

    Add Redshift config

---
+++ redshift/files/redshift/hooks/brightness.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+brightness_day=100
+brightness_transition=90
+brightness_night=80
+fade_time=60000
+fps=60
+
+case $1 in
+    period-changed)
+        case $3 in
+            daytime)
+                xbacklight -set $brightness_day -time $fade_time -fps $fps
+                ;;
+            transition)
+                xbacklight -set $brightness_transition -time $fade_time -fps $fps
+                ;;
+            night)
+                xbacklight -set $brightness_night -time $fade_time -fps $fps
+                ;;
+        esac
+        ;;
+esac
