87e86dc1e976ea6df7a5b67e40a93a64fa35cac0
Ada Joule <ada.fulmina@gmail.com>
2018-02-05 00:52:50 +0700

    Change TallHalf layout to 2 main windows

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -44,10 +44,10 @@ main = xmonad =<< statusBar myStatusBar myPP myToggleStruts
     )
 
 myLayoutHook =
-    fullscreenFocus $
+    fullscreenFull $
     onWorkspaces [ "2 web", "8 office", "9 ide" ] (spacingWithEdge 3 (tallTwoThird ||| Mirror tallTwoThird ||| Full)) $
     onWorkspaces [ "3 game", "4 media", "5 vm" ] (spacingWithEdge 3 tallHalf ||| smartBorders Full) $
     (spacingWithEdge 3 (tallHalf ||| Mirror tallHalf ||| Grid ||| Full))
-tallHalf = Tall 1 (3/100) (1/2)
+tallHalf = Tall 2 (3/100) (1/2)
 tallTwoThird = Tall 1 (3/100) (2/3)
 
