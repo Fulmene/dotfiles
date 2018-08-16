7778bfb4dff1eb24fcbd556955992e02aa35b440
Ada Joule <ada.fulmina@gmail.com>
2018-08-16 22:30:00 +0700

    Remove fullScreenSupport from XMonad
    Breaks with 0.14

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -10,8 +10,6 @@ import XMonad.Layout.NoBorders
 import XMonad.Layout.Spacing
 import XMonad.Layout.Fullscreen
 
-import qualified XMonad.StackSet as W
-
 import XMonad.Util.EZConfig
 
 import XMonad.Fulmene.Applications
@@ -19,8 +17,27 @@ import XMonad.Fulmene.KeyBindings
 import XMonad.Fulmene.Management
 import XMonad.Fulmene.StatusBar
 
-import Data.List
+main = xmonad =<< statusBar myStatusBar myPP myToggleStruts
+    (   withUrgencyHook NoUrgencyHook $
+        ewmh def
+            {   terminal            = myTerminal ,
+                focusFollowsMouse   = False ,
+                clickJustFocuses    = False ,
 
+                borderWidth         = 1 ,
+                normalBorderColor   = "#303030" ,
+                focusedBorderColor  = "#DFDFAF" ,
+
+                workspaces          = myWorkspaces ,
+                modMask             = myModMask ,
+
+                layoutHook          = myLayoutHook ,
+                manageHook          = myManageHook
+            }
+        `removeKeysP` myRemoveKeys
+        `additionalKeysP` myKeys
+    )
+{-
 main = xmonad =<< statusBar myStatusBar myPP myToggleStruts
     (   withUrgencyHook NoUrgencyHook $
         fullscreenSupport $
@@ -42,12 +59,14 @@ main = xmonad =<< statusBar myStatusBar myPP myToggleStruts
         `removeKeysP` myRemoveKeys
         `additionalKeysP` myKeys
     )
-
+-}
 myLayoutHook =
-    fullscreenFull $
-    onWorkspaces [ "2 web", "8 office", "9 ide" ] (spacingWithEdge 3 (tallTwoThird ||| Full)) $
-    onWorkspaces [ "3 game", "4 media", "5 vm" ] (spacingWithEdge 3 tallTwoThird ||| smartBorders Full) $
-    (spacingWithEdge 3 (tallHalf ||| Full))
+    --fullscreenFull $
+    onWorkspaces [ "2 web", "8 office", "9 ide" ] (mySpacing (tallTwoThird ||| Full)) $
+    onWorkspaces [ "3 game", "4 media", "5 vm" ] (mySpacing tallTwoThird ||| smartBorders Full) $
+    (mySpacing (tallHalf ||| Full))
 tallHalf = Tall 2 (3/100) (1/2)
 tallTwoThird = Tall 1 (3/100) (2/3)
+
+mySpacing = spacingRaw False (Border 2 2 2 2) True (Border 2 2 2 2) True
 
