ee51717104a8ac311efb895bc7e73ff2f0aacad5
Ada Joule <ada.fulmina@gmail.com>
2019-03-18 20:14:28 +0700

    Remove dein installation file

--- install-dein.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-plugin_dir=~/.config/nvim/dein
-
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > $plugin_dir/install_dein.sh
-sh $plugin_dir/install_dein.sh $plugin_dir
-
