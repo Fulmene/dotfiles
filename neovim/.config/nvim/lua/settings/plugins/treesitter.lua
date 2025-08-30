178243363a7bd588447d97a5e452d87abf713507
Ada Joule <ada.fulmina@gmail.com>
2025-08-30 18:17:58 +0700

    Config treesitter folding

--- neovim/.config/nvim/lua/settings/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/settings/plugins/treesitter.lua
@@ -5,6 +5,9 @@ function TreeSitter.setup()
     require('nvim-treesitter.configs').setup {
         sync_install = false,
         auto_install = true,
+        folds = {
+            enable = true;
+        },
         highlight = {
             enable = true;
             additional_vim_regex_highlighting = false;
@@ -15,7 +18,9 @@ function TreeSitter.setup()
     }
 
     vim.opt.foldmethod = 'expr'
-    vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+    vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    vim.opt.foldtext = ""
+    vim.opt.foldlevel = 3
 end
 
 return TreeSitter
