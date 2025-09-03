d636e7e09bbc4bce6d01f8137cbaaee5695fcbdf
Ada Joule <ada.fulmina@gmail.com>
2025-09-03 18:10:46 +0700

    Switch to pyrefly + ruff lsp

--- neovim/.config/nvim/lua/conf/plugins/lspconfig.lua
+++ neovim/.config/nvim/lua/conf/plugins/lspconfig.lua
@@ -4,7 +4,8 @@ LspConfig = {}
 function LspConfig.setup()
     local servers = {
         'lua_ls',
-        'basedpyright',
+        'pyrefly',
+        'ruff',
         'ts_ls',
         'cssls',
         'jsonls',
@@ -14,6 +15,17 @@ function LspConfig.setup()
     for _, ls in ipairs(servers) do
         vim.lsp.enable(ls)
     end
+
+    vim.lsp.config('ruff', {
+        init_options = {
+            settings = {
+                lineLength = 100,
+                lint = {
+                    enable = false,
+                },
+            }
+        }
+    })
 end
 
 return LspConfig
